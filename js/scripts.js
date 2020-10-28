a2a84e15183d4bc7b3826424d67bf503ab148459
Jon Maldia <jon@maldia.net>
2020-10-27 22:03:28 -0400

    Use promise instead

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,14 @@
-var choreEarnedDataURL = 'https://api.sheety.co/d0224c1122eba3307dc190260ea81f6c/cadenceRewards/reward';
-// var choreEarnedDataURL = process.env.SHEETYAPI;
+let url = 'https://api.sheety.co/d0224c1122eba3307dc190260ea81f6c/cadenceRewards/reward';
 
-function httpGet(theUrl) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
+fetch(url)
+    .then((response) => response.json())
+    .then(json => {
+        // Do something with the data
+        let earnedCash = json.reward[0].amount.toFixed(2);
+        let earnedPass = json.reward[10].amount;
 
-// CONVERT RETURN VALUE TO JSON
-var dataJSON = JSON.parse(httpGet(choreEarnedDataURL));
-
-// VARIABLE TO BE DISPLAYED
-var earnedCash = dataJSON.reward[0].amount.toFixed(2);
-var earnedPass = dataJSON.reward[10].amount;
-
-// DISPLAY DATA ON PAGE
-document.getElementById("earned-cash").innerHTML = "$" + earnedCash;
-document.getElementById("earned-pass").innerHTML = earnedPass;
-
-console.log('START HERE:')
-// console.log(dataJSON.reward);
-console.log(typeof earnedCash);
-console.log(earnedPass);
+        document.getElementById("earned-cash").innerHTML = "$" + earnedCash;
+        document.getElementById("earned-pass").innerHTML = earnedPass;
+        
+        return json.reward;
+    });
